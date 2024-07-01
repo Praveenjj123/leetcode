15493ae402045ae8c1705d580c1443d32ecd3499
Praveenjj123 <160823010+Praveenjj123@users.noreply.github.com>
2024-07-01 12:45:38 +0530

    Time: 1 ms (71.09%), Space: 41.4 MB (6.32%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        // double z=(double)dividend/(double)divisor;
+        return (int)((double)dividend/(double)divisor);
+    }
+}
