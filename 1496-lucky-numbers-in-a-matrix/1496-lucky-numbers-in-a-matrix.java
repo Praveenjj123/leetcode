dade5a4cc9304ec27259db37a599071afd99e700
Praveenjj123 <160823010+Praveenjj123@users.noreply.github.com>
2024-07-21 11:26:32 +0530

    Time: 14 ms (5.4%), Space: 45.2 MB (64.63%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+ int i,j,r,c,k;
+ int max=Integer.MIN_VALUE;
+ int min=Integer.MAX_VALUE;
+ r=matrix.length;
+ c=matrix[0].length;
+ List<Integer> list=new ArrayList<>();
+ int[] one=new int[r];
+ int[] two=new int[c];
+ i=0;
+ k=0;
+ for(i=0;i<r;i++)
+ {
+    for(j=0;j<c;j++)
+    {
+       if(matrix[i][j]<min)
+       {
+           min=matrix[i][j];
+       }
+    }
+    one[k++]=min;
+    min=Integer.MAX_VALUE;
+} 
+k=0;
+
+for(i=0;i<c;i++)
+ {
+    for(j=0;j<r;j++)
+    {
+       if(matrix[j][i]>max)
+       {
+           max=matrix[j][i];
+       }
+    }
+    two[k++]=max;
+    max=Integer.MIN_VALUE;
+} 
+for(i=0;i<c;i++)
+{
+    System.out.println(two[i]);
+}
+for(i=0;i<r;i++)
+{
+    System.out.println(one[i]);
+}
+for(i=0;i<r;i++)
+ {
+    for(j=0;j<c;j++)
+    {    
+if(one[i]==two[j])
+{
+    list.add(one[i]);
+}
+    }
+}
+return list;
+    }
+}
